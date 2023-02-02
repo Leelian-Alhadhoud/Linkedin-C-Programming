75a2f111fedb87eec78e8052852782aefaed8a40
Leelian-waleed <114055532+Leelian-waleed@users.noreply.github.com>
2023-02-02 15:41:11 +0400

    will it work

---
+++ variables3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float pi ;
+
+    pi = 22.00 / 7.0;
+    printf("The ancients calculated PI as %f.\n" , pi );
+    /* %f is the placeholder of the float number of pi */ 
+
+    return (0);
+
+}
