02692b81d16fe6ce4423efd6b1b0fda5dcb8631a
Leelian-waleed <114055532+Leelian-waleed@users.noreply.github.com>
2023-02-08 09:28:27 +0400

    chapter 3 return functions

---
+++ return3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float product(float a, float b, float c);
+
+int main()
+{
+	float a,x,y,z;
+
+	printf("Type three numbers, separated by spaces: ");
+	scanf("%f %f %f",&x,&y,&z);
+	a = product(x,y,z);
+	printf("%f * %f * %f = %f\n",x,y,z,a);
+
+	return(0);
+}
+
+float product(float a, float b, float c)
+{
+	float p;
+
+	p = a * b * c;
+	return(p);
+    // return(a * b * c);
+    // another way to do it in one line 
+}
+
